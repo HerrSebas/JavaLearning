40bdc1781bf7ee65320b84657df1ff01ddf8bd46
HerrSebas <sebnartor@gmail.com>
2022-11-21 22:49:54 -0500

    concatenation

--- Fundamentals/HelloWorldJava/src/helloworldjava/HelloWorldJava.java
+++ Fundamentals/HelloWorldJava/src/helloworldjava/HelloWorldJava.java
@@ -4,6 +4,8 @@
  */
 package helloworldjava;
 
+import javax.print.attribute.SupportedValuesAttribute;
+
 /**
  *
  * @author sebna
@@ -14,8 +16,26 @@ public class HelloWorldJava {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        // TODO code application logic here
-        System.out.println("Hola mundo");
+        // //int miVariableEntera = 10;
+        // //System.out.println(miVariableEntera);
+        // miVariableEntera = miVariableEntera + 5;
+        // System.out.println(miVariableEntera);
+
+        // String miVariableString = "Mi nombre es Sebastian";
+        // System.out.println(miVariableString);
+        // miVariableString = "Y mi apellido es Naranjo";
+        // System.out.println(miVariableString);
+        
+        // var myVariable = "esto es una variable inferida";
+        // System.out.println(myVariable);
+
+        // // permitted ways to name a variable
+        // var $myVariable = 2;
+        // var _myVariable = 3;
+
+        var usuario = "juan";
+        var
     }
+
     
 }
